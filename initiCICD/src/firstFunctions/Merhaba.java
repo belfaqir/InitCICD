d4e26385c8d072da4732d2879f8e30607a1c1678
belfaqir <belfaqir_imaf@yahoo.fr>
2021-02-02 01:36:44 +0100

    update message

--- initiCICD/src/firstFunctions/Merhaba.java
+++ initiCICD/src/firstFunctions/Merhaba.java
@@ -5,7 +5,7 @@ public class Merhaba {
 	public static void main(String[] args) {
 
 for(int i=1;i<10;i++) {
-	System.out.println("Merhaba folk");
+	System.out.println("Merhaba folk, i love you");
 }
 	}
 
